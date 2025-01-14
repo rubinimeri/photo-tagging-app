be4957fcea6f410e3b6b7eb93b532d9591c92a04
rubin <rubinimeri6@gmail.com>
2025-01-14 11:32:54 +0100

    Add constants file

---
+++ frontend/src/constants.ts
@@ -0,0 +1,22 @@
+export const CROSSHAIR_WIDTH = 22;
+export const CROSSHAIR_HEIGHT = 22;
+export const TARGETING_BOX_WIDTH = 100;
+export const TARGETING_BOX_HEIGHT = 100;
+
+export const EXAMPLE_CHARACTERS = [
+  {
+    id: 1,
+    url: "https://github.com/shadcn.png",
+    name: "Character 1",
+  },
+  {
+    id: 2,
+    url: "https://github.com/shadcn.png",
+    name: "Character 2",
+  },
+  {
+    id: 3,
+    url: "https://github.com/shadcn.png",
+    name: "Character 3",
+  },
+];

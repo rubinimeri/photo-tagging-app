822a2dcab23a2118042b524bcb7a5c151d0c5aaa
rubin <rubinimeri6@gmail.com>
2025-01-15 17:20:42 +0100

    Add types.ts file

---
+++ frontend/src/types.ts
@@ -0,0 +1,15 @@
+export type Coordinates = {
+  x: number | null;
+  y: number | null;
+};
+
+export type Game = {
+  id: number;
+  name: string;
+  url: string;
+};
+
+export type Character = {
+  url: string;
+  name: string;
+};

e0bb32d7c0e73c84d65048850ff71d957d2121b7
Rubin Imeri <rubinimeri6@gmail.com>
2025-01-21 21:33:12 +0100

    Fix bug

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,6 @@
 export const CROSSHAIR_WIDTH = 43;
 export const CROSSHAIR_HEIGHT = 43;
-export const TARGETING_BOX_WIDTH = 213;
+export const TARGETING_BOX_WIDTH = 220;
 export const TARGETING_BOX_HEIGHT = 171.2;
 
 export const EXAMPLE_CHARACTERS = [
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,9 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
 });
